fe4d7ffe3ef9e4827e4b99f8f657e49564131ca4
Astrid Rosal <astridjamesrosal95@gmail.com>
2026-04-19 22:26:26 +0800

    Adding comments based on understanding to not forget. P2

--- expensetracker.py
+++ expensetracker.py
@@ -14,9 +14,9 @@ def save_expenses():                            #Saves the current expenses list
 expenses = load_expenses()                      #Instead of [], we will load previously saved expenses from the JSON file.
 next_id = 1                                     #Initialize next_id to 1, which will be used to assign unique IDs to expenses. 
 
-def show_menu():
+def show_menu():                                #Creates a function that shows the menu options the user can choose.
 
-    print('Hello, Please select a number')
+    print('Hello, Please select a number')      
     print('1. Add Expense')
     print('2. View Expenses')
     print('3. Remove Expenses')
@@ -24,27 +24,27 @@ def show_menu():
     print('5. View Expenses by Category')
     print('6. Exit')
 
-def add_expense():
-    global next_id
+def add_expense():                              #Creates a function that allows the user to add an expense.            
+    global next_id                              #When modifying, we declare it as global to ensure we change not create a new local variable.
+   
+    while True:                                 #Repeats asking the user for an amount until they provide a valid one.
+        amount = input("Enter amount: ")        #Ask the user to ask the amount for their expense.
+        if amount.replace('.', '', 1).isdigit():    #If we enter a decimal, it would still be valid because of the replace, and it temporarily removes the decimal from the number, and is.digit checks it as a valid number
+            break                                   #Once input is valid, it breaks the loop and proceeds to ask for category and description.
+        print("Invalid input, please enter a real amount.") 
 
-    while True:
-        amount = input("Enter amount: ")
-        if amount.replace('.', '', 1).isdigit():
-            break
-        print("Invalid input, please enter a real amount.")
-
-    category = input("Enter category: ")
-    description = input("Enter description: ")
+    category = input("Enter category: ")        #Ask the user for the category of their expense. No Validation Loop because there are no wrong answer.
+    description = input("Enter description: ")  #Ask the user for a description of their expense. No Validation Loop because there are no wrong answer.
 
     expense = {
-        "id": next_id,
-        "amount": float(amount),
-        "category": category,
-        "description": description
-    }
+        "id": next_id,                          #The ID is assigned from the next_id which starts at 1 to ensure unique identifier.
+        "amount": float(amount),                #Float converts the amount from a string to a number with decimals for accurate math operations or procedures later.
+        "category": category,                   #Category of the expense provided by the user.
+        "description": description              #Description of the expense provided by the user.
+    }   
 
-    expenses.append(expense)
-    next_id += 1
+    expenses.append(expense)                    #Append means adding the new "expense" to the end of the expenses list.
+    next_id += 1                                #When the number of expenses increase, the id number assigned to them also increases.
     print("Expense added successfully!")
 
 def view_expenses():
